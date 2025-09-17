67342e811e1e5d0b9b8fbf25ed5e5740db6933c7
Mafthiyar <mafthyar@gmail.com>
2025-09-17 18:04:15 +0530

    oddsum

--- Methods/sumofodd.java
+++ Methods/sumofodd.java
@@ -1,3 +1,22 @@
-public class sumofodd {
-    a
+import java.util.*;
+class sumofodd{
+    public static void Myodd(int n,int sum) {
+     for(int i=1; i<=n; i++){
+         if(i % 2 != 0) {
+             sum = sum + i;
+         }
+
+     }System.out.println(sum);
+
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        Myodd(n,sum);
+
+    }
+
+
 }
