e4ddc31a167dbf398baf3cdb65d2ca37b561bb62
Mafthiyar <mafthyar@gmail.com>
2025-09-18 13:30:13 +0530

    twosum

---
+++ Methods/Mytwosum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sumofodd{
+    public static void Mytwosum(int n,int m) {
+       int k = n + m;
+     System.out.println(k);
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int m  =sc.nextInt();
+
+
+        Mytwosum(n,m);
+
+    }
+
+
+}
