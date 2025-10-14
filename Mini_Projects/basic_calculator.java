7b640be1ba2e48a931da35f3080cfbb8fe098bdd
Mafthiyar <mafthyar@gmail.com>
2025-10-14 14:08:22 +0530

    Basic_Calculator

---
+++ Mini_Projects/basic_calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Calculator{
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the first number :");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the operator ( +, -, *, / ) : ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter the second number :");
+        int num2 = sc.nextInt();
+
+        double result=0;
+
+        switch(operator){
+            case'+' :
+                result = num1 + num2;
+                break;
+
+            case'-' :
+                result = num1 - num2;
+                break;
+
+            case'*' :
+                result = num1 * num2;
+                break;
+
+            case'/' :
+                if( num2 != 0){
+                    result = num1 / num2;
+                }else{
+                    System.out.println("Error ! : division by zero ");
+                }
+                break;
+
+        }
+
+        System.out.println(" result : " + result);
+
+    }
+}
